1a1d1adc9c8c4496b52e50d789584016746d57dd
rogers <mwangicarolyne@zetech.ac.ke>
2022-01-21 16:26:51 +0300

    initial commit

---
+++ hello.py
@@ -0,0 +1,8 @@
+#print hello world
+print("hello world ")
+print("I am Rogers Mwangi")
+print(" this is my first python class")
+
+
+
+
---
+++ triangle.py
@@ -0,0 +1,10 @@
+#area of a triangle
+base = int(input("Enter the base:"))
+height = int(input("Enter the height:"))
+
+area=1/2*base*height
+print("The area is ",area)
+
+
+
+
